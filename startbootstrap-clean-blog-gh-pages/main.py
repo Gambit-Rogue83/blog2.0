c91be934ad81b348978ff7e21f1214adc32ba1a3
Shattuck Kuzmic <shattuckkuzmic@gmail.com>
2024-12-07 16:39:30 -0700

    Form data

--- startbootstrap-clean-blog-gh-pages/main.py
+++ startbootstrap-clean-blog-gh-pages/main.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 posts = requests.get("https://api.npoint.io/674f5423f73deab1e9a7").json()
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def get_all_posts():
     return render_template("index.html", all_posts=posts)
 
+
 @app.route('/about')
 def about():
     return render_template("about.html")
+
 
 @app.route('/post/<int:index>')
 def show_post(index):
@@ -21,9 +24,19 @@ def show_post(index):
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
-@app.route('/contact')
+
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        data = request.form
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+    # name = request.form['name']
+    # email = request.form['email']
+    # phone = request.form['phone']
+    # message = request.form['message']
+    # print(f"{name}\n, {email}\n, {phone}\n, {message}\n")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
